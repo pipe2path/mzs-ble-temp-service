e25793b25c7d0b5fe3a6338c9c150b83d245aea3
kevinmenezes <kevinmenezes@yahoo.com>
2020-05-17 16:48:57 -0700

    modified app.js for heroku

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tmpRouter = require('./routes/temperature');
